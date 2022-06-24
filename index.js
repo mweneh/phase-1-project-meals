6cddb237dd804366b89754c966f76e48d578f354
David Ongaro <davidongaroh@gmil.com>
2022-06-24 11:55:17 +0300

    comment button & js

---
+++ index.js
@@ -0,0 +1,20 @@
+let textArea= document.querySelector('textarea');
+const backUp = feild.getAttribute('placeholder');
+const btn = document.querySelector('.btn');
+const clear = document.getElementById('clear')
+
+textArea.onfocus = function(){
+    this.setAttribute('placeholder', '');
+    this.style.borderColor = '#333';
+    btn.style.display = 'block'
+}
+
+textArea.onblur = function(){
+    this.setAttribute('placeholder',backUp);
+    this.style.borderColor = '#aaa'
+}
+
+clear.onclick = function(){
+    btn.style.display = 'none';
+    textArea.value = '';
+}
