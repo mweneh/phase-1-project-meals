127ec0c889dbcb7b7c9c09b1060ab80bec7b9581
David Ongaro <davidongaroh@gmil.com>
2022-06-24 12:28:47 +0300

    DOM elements

--- index.js
+++ index.js
@@ -3,6 +3,12 @@ const backUp = textArea.getAttribute('placeholder');
 const btn = document.querySelector('.btn');
 const clear = document.getElementById('clear')
 
+const searchButton = document.querySelector('#search-button');
+const mealList = document.getElementById('food');
+const mealContent = document.querySelector('.details-content');
+const recipeCloseBtn = document.querySelector('#recipe-close-btn');
+const recipeBtn = document.querySelector('.recipe-btn')
+
 textArea.onfocus = function(){
     this.setAttribute('placeholder', '');
     this.style.borderColor = '#333';
