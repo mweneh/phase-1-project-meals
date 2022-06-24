d3e0fdd91479906fec5d160d07fe912031a15bec
David Ongaro <davidongaroh@gmil.com>
2022-06-24 12:32:24 +0300

    fetching mealList

--- index.js
+++ index.js
@@ -24,3 +24,42 @@ clear.onclick = function(){
     btn.style.display = 'none';
     textArea.value = '';
 }
+// get meal list that matches name 
+function fetchMealList(){
+    let searchInput = document.getElementById('search-input').value.trim();
+    fetch((`https://www.themealdb.com/api/json/v1/1/search.php?s=${searchInput}`))
+    .then(response => response.json())
+    .then(data => {
+        let html = "";
+        if(data.meals){
+            data.meals.forEach(meal => {
+                html += `
+                    <div class = "meal-item" data-id = "${meal.idMeal}">
+                        <div class = "meal-img">
+                            <img src = "${meal.strMealThumb}" alt = "meal">
+                        </div>
+                        <div class = "meal-name">
+                            <h3>Name: ${meal.strMeal}</h3>
+                            <p><strong> Category: ${meal.strCategory}</strong></p>
+                            <p><strong> Area: ${meal.strArea}</strong></p>
+                            <h3>Instructions:</h3>
+                            <p>${meal.strInstructions}</p>
+                            <div class = "recipe-link">
+                                <a href = "${meal.strYoutube}" target = "_blank">Watch Video instead?</a>
+                            </div>
+
+                            <a href = "#" class = "recipe-btn">Order Meal</a>
+                            <a href = '@' class = "submit-btn"> Add to Favorites</a>
+                        </div>
+                    </div>
+                `;
+            });
+            mealList.classList.remove('notFound');
+        } else{
+            html = "Sorry, no match found!";
+            mealList.classList.add('notFound');
+        }
+
+        mealList.innerHTML = html;
+    });
+}
